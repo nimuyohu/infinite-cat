54e652c74d9e6859bd57f90b98fab21278b4d7d2
nimurarikuto <nimuyohu0105@gmail.com>
2021-01-20 01:49:42 +0900

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import {useState} from 'react';
-import InfiniteScroll  from "react-infinite-scroller"
+import { useState } from 'react';
+import InfiniteScroll from "react-infinite-scroller"
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import Skeleton from '@material-ui/lab/Skeleton';
+import ElevationScroll from './ElevationScroll';
+
 
 function App() {
   const useStyles = makeStyles((theme) => ({
@@ -19,7 +21,7 @@ function App() {
     },
   }));
   const classes = useStyles();
-  
+
   // 表示するデータ
   const [list, setList] = useState([]);
   // 再読み込み判定
@@ -31,7 +33,7 @@ function App() {
     // 猫ちゃんの画像API呼び出し
     const response = await fetch('https://api.thecatapi.com/v1/images/search');  //API通信
     const data = await response.json();  //取得データ
-    
+
     //データ件数が0件の場合、処理終了
     if (data.length < 1) {
       setHasMore(false);
@@ -39,35 +41,35 @@ function App() {
     }
 
     //取得データをリストに追加
-    setList([...list,...data])
+    setList([...list, ...data])
   }
 
   //各スクロール要素
   const items = (
     <div className={classes.root}>
-    <GridList cellHeight={200} className={classes.gridList} cols={2}>
-      {list.map((value,index) => (
-        <GridListTile key={index}>
-            <img src={value.url} alt='cat' key={index}/>
-            <Skeleton variant="rect"  height={500} />
-        </GridListTile>
-      ))}
-    </GridList>
-    
+      <ElevationScroll />
+      <GridList cellHeight={200} className={classes.gridList} cols={2}>
+        {list.map((value, index) => (
+          <GridListTile key={index}>
+            <img src={value.url} alt='cat' key={index} />
+            <Skeleton variant="rect" height={500} />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
-    );
-    
-    //ロード中に表示する項目
-    const loader =<div className="loader" key={0}></div>;
-    
-    return (
-      <div>
+  );
+
+  //ロード中に表示する項目
+  const loader = <div className="loader" key={0}></div>;
+
+  return (
+    <div>
       <InfiniteScroll
         loadMore={loadMore}    //項目を読み込む際に処理するコールバック関数
         hasMore={hasMore}      //読み込みを行うかどうかの判定
         loader={loader}
-        >      {/* 読み込み最中に表示する項目 */}
-          {items}             {/* 無限スクロールで表示する項目 */}
+      >      {/* 読み込み最中に表示する項目 */}
+        {items}             {/* 無限スクロールで表示する項目 */}
       </InfiniteScroll>
     </div>
   );
